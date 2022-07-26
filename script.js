8ba3937168bda35c8a9517ff99dae49a53746436
wdrbork <wdrb2001@gmail.com>
2022-07-26 14:32:30 -0700

    Create function that simulates 5 rounds

--- script.js
+++ script.js
@@ -18,6 +18,23 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
+function game() {
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = promptUser();
+        let computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+    }
+}
+
+function promptUser() {
+    let choice = capitalize(prompt("What is your choice? "))
+    while (!OPTIONS.includes(choice)) {
+        choice = capitalize(prompt("Invalid choice! Please try again" ));
+    }
+
+    return choice;
+}
+
 function capitalize(selection) {
     return selection.substring(0, 1).toUpperCase() + 
         selection.substring(1).toLowerCase();
