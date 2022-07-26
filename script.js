8b475b0071f7664792302f6c7b89932f0dede188
wdrbork <wdrb2001@gmail.com>
2022-07-26 14:16:02 -0700

    Create function that simulates a round
    Also added a function that capitalizes the player's choice to match
    it with the capitalization of the options in the underlying array.

--- script.js
+++ script.js
@@ -3,3 +3,22 @@ const OPTIONS = ["Rock", "Paper", "Scissors"];
 function getComputerChoice() {
     return OPTIONS[Math.floor(Math.random() * 3)];
 }
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return `It's a tie! ${playerSelection} ties with ${computerSelection}`;
+    }
+    
+    if (playerSelection === "Rock" && computerSelection === "Scissors" || 
+            playerSelection === "Paper" && computerSelection === "Rock" || 
+            playerSelection === "Scissors" && computerSelection === "Paper") {
+        return `You win! ${playerSelection} beats ${computerSelection}`;
+    } else {
+        return `You lose! ${computerSelection} beats ${playerSelection}`;
+    }
+}
+
+function capitalize(selection) {
+    return selection.substring(0, 1).toUpperCase() + 
+        selection.substring(1).toLowerCase();
+}
